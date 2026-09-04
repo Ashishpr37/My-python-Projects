11e00fe19a25043f3848c0612a54349fe2b6fa35
Secretive plotter <ashish768924@gmail.com>
2026-09-04 16:36:58 +0530

    Program 3

---
+++ calculator.py
@@ -0,0 +1,13 @@
+print ("************** Welcome to KG Calculator ********************")
+a= int(input("Enter first number : "))
+
+b= int(input("Enter Second number: "))
+
+print("First number: ", a,"\nSecond number :",b)
+
+print(a, "+", b, "=", (a+b) )
+print(a , "-", b, "=" ,(a-b) )
+print(a , "*", b, "=" ,(a*b) )
+print(a , "/", b, "=" ,(a/b) )
+print(a , "%", b, "=" ,(a%b) )
+print(a , "//", b, "=" ,(a//b) )

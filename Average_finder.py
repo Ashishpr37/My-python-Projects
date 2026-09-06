d57540f04e6bcf0b5868a86f686aa3bc6d7ce640
Secretive plotter <ashish768924@gmail.com>
2026-09-07 02:46:33 +0530

    Program 6

---
+++ Average_finder.py
@@ -0,0 +1,12 @@
+#Project of average of 2 numbers
+a=int(input("Enter number 1 : "))
+
+b=int(input("Enter number 2 : "))
+
+print ("These are the numbers you entered:",a,",",b)
+
+print(a,"+",b,"/2=?")
+
+c = a+b
+
+print("The average is :",a,"+",b,"/2=",c/2)
